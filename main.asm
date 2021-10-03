881902e4cd65ee152f40071bfb5be3d6322b2de3
vra1973 <vasylenkora@aol.com>
2021-10-03 20:16:10 +0200

    Set_Size_Console

--- main.asm
+++ main.asm
@@ -3,17 +3,41 @@ include main.inc
 
 .code
 start:
-
-  fn Main
-  ;----------
-  inkey
-  ;----------
-  exit
+    ; set size and title
+    fn SetConsoleTitle,"Snake demo"
+    fn SetWindowSize,MAX_WIDTH,MAX_HEIGHT
+    
+    
+    
+    fn Main
+    ;----------
+    inkey
+    ;----------
+    exit
 ;********************
 Main proc
 
 
     ret
 Main endp
+;********************
+SetWindowSize    proc uses ebx esi edi wd: DWORD,ht:DWORD
+    fn GetStdHandle, -11
+    ;---------------
+    push eax
+    ;---------------
+    mov ebx,ht
+    shl ebx,16
+    ;---------------
+    or ebx,wd
+    ;--------------
+    fn SetConsoleScreenBufferSize, eax, ebx
+    ;---------------
+    pop eax
+    ;---------------
+    fn SetConsoleWindowInfo,eax,1,offset srect
+    
+    ret
+SetWindowSize endp
 
 end start
