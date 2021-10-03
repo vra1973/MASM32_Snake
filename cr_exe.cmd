5a1bf87e867681255a8c93d5a9850cc7b82a469c
vra1973 <vasylenkora@aol.com>
2021-10-03 19:04:19 +0200

    Shablon_01

---
+++ cr_exe.cmd
@@ -0,0 +1,6 @@
+ml /c /coff main.asm
+pause
+
+link /subsystem:console main.obj
+
+pause
---
+++ main.asm
@@ -0,0 +1,19 @@
+include main.inc
+
+
+.code
+start:
+
+  fn Main
+  ;----------
+  inkey
+  ;----------
+  exit
+;********************
+Main proc
+
+
+    ret
+Main endp
+
+end start
